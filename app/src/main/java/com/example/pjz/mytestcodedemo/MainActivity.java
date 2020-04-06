3edabcfedae6d338ae0aba47745125c6d77513f9
Rocky <13821266232@163.com>
2020-04-06 16:20:08 +0800

    add logic code to mainActivity for toast show

--- app/src/main/java/com/example/pjz/mytestcodedemo/MainActivity.java
+++ app/src/main/java/com/example/pjz/mytestcodedemo/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.pjz.mytestcodedemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener{
+    private Button mGetToastBt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        mGetToastBt.setOnClickListener(this);
+    }
+
+    private void initView() {
+        mGetToastBt = (Button) findViewById(R.id.get_toast);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.get_toast:
+                show("this is test demo");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void show(String txt) {
+        Toast.makeText(this,txt,Toast.LENGTH_SHORT).show();
     }
 }
